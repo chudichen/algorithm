7f004803a67c8b905532c646e0b1cf50646dd109
Michael.Chu <chudichen@foxmail.com>
2019-03-20 16:23:45 +0800

    添加反转链表

---
+++ linked_list/reverse_list.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+"""
+https://leetcode.com/explore/featured/card/recursion-i/251/scenario-i-recurrence-relation/2378/
+解题思路将使用虚拟before指针，将head的值依次传递给before，最终形成反转链表。
+"""
+
+
+class Solution:
+
+    def reverse_list(self, head: ListNode) -> ListNode:
+        before = None
+        while head:
+            temp = head.next
+            head.next = before
+            before = head
+            head = temp
+        return before
+
+
+# 测试方法
+if __name__ == '__main__':
+    headNode = ListNode(1)
+    add = headNode
+    for i in range(2, 6):
+        add.next = ListNode(i)
+        add = add.next
+
+    solution = Solution()
+    solution.reverse_list(headNode)
+
