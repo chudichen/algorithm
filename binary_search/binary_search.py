60e9fb3972e3e30064c3b3c1c490e913cc410a22
chudichen <chudichen.foxmail.com>
2019-05-16 17:37:40 +0800

    二分搜索

---
+++ binary_search/binary_search.py
@@ -0,0 +1,21 @@
+""""
+二分搜索
+
+https://leetcode.com/explore/learn/card/binary-search/138/background/1038/
+"""
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l,r = 0,len(nums)-1
+        while l<=r:
+            mid = (r+l)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid+1
+            else:
+                r = mid-1
+        return -1
+
