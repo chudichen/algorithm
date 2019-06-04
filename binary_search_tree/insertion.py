01ec38408434925b1e1725f38bb6fce0f84b17d6
chudichen <chudichen.foxmail.com>
2019-06-04 11:34:20 +0800

    二分搜索树添加节点

---
+++ binary_search_tree/insertion.py
@@ -0,0 +1,24 @@
+"""
+二分搜索树插入
+
+https://leetcode.com/explore/learn/card/introduction-to-data-structure-binary-search-tree/141/basic-operations-in-a-bst/1003
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return TreeNode(val)
+        if root.val < val:
+            root.right = self.insertIntoBST(root.right, val)
+        else:
+            root.left = self.insertIntoBST(root.left, val)
+        return root
